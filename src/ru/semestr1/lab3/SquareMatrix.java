bd91b59617e53320f69c421fdaf3729fcb47ceff
LordLizard <WarriorXKz@gmail.com>
2023-02-07 22:16:36 +0300

    Make SquareMatrrix

---
+++ src/ru/semestr1/lab3/SquareMatrix.java
@@ -0,0 +1,30 @@
+package ru.semestr1.lab3;
+
+public class SquareMatrix extends Matrix {
+    public SquareMatrix(int size) {
+        super(size, size);
+        for (int i = 0; i < size; i++) {
+            this.matrix[i][i] = 1;
+        }
+    }
+
+    public SquareMatrix() {
+        this(1);
+    }
+
+    public SquareMatrix sum(SquareMatrix matrix) {
+        if (this.rows != matrix.rows) {
+            throw new MatrixException("Matrices have different sizes");
+        }
+
+        SquareMatrix result = new SquareMatrix(this.rows);
+
+        for (int i = 0; i < this.rows; i++) {
+            for (int j = 0; j < this.columns; j++) {
+                result.matrix[i][j] = this.matrix[i][j] + matrix.matrix[i][j];
+            }
+        }
+
+        return result;
+    }
+}
