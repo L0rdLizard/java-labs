b5ece6693226f1811c6c523a8a0574130b3c5e6b
LordLizard <WarriorXKz@gmail.com>
2023-02-16 11:21:32 +0300

    lab1 dop

---
+++ src/ru/semestr1/lab1/Fraction.java
@@ -0,0 +1,44 @@
+package ru.semestr1.lab1;
+
+public class Fraction {
+    private int numerator;
+    private int denominator;
+
+    public Fraction() {
+        this(1, 1);
+    }
+
+    public Fraction(int numerator) {
+        this(numerator, 1);
+    }
+
+    public Fraction(int numerator, int denominator) {
+        this.numerator = numerator;
+        this.denominator = denominator;
+    }
+
+    public int getNumerator() {
+        return numerator;
+    }
+
+    public int getDenominator() {
+        return denominator;
+    }
+
+
+    public Fraction add(Fraction other) {
+        int newNumerator = numerator * other.denominator + other.numerator * denominator;
+        int newDenominator = denominator * other.denominator;
+        return new Fraction(newNumerator, newDenominator);
+    }
+
+    public Fraction mul(Fraction other) {
+        int newNumerator = numerator * other.numerator;
+        int newDenominator = denominator * other.denominator;
+        return new Fraction(newNumerator, newDenominator);
+    }
+
+    public String toString(){
+        return this.numerator + "/" + this.denominator;
+    }
+}
