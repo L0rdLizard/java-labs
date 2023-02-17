eaae78545c86314507ac825fcc8baa7e2da5621a
LordLizard <WarriorXKz@gmail.com>
2023-02-17 20:45:48 +0300

    lab4 start

--- src/ru/semestr1/lab04/SortedIntegerList.java
+++ src/ru/semestr1/lab04/SortedIntegerList.java
@@ -1,5 +1,35 @@
 package ru.semestr1.lab04;
 
+import java.util.LinkedList;
+import java.util.ListIterator;
+
 public class SortedIntegerList {
+    private LinkedList<Integer> list;
+    boolean IsDublicates;
+
+    public SortedIntegerList(boolean IsDublicates) {
+        this.IsDublicates = IsDublicates;
+        list = new LinkedList<Integer>();
+    }
+
+    public SortedIntegerList() {
+        // this(true);
+        this.IsDublicates = true;
+    }
     
+    public void add(int value){
+        if (list.isEmpty()){
+            list.add(value);
+        }
+        else{
+            ListIterator<Integer> iterator = list.listIterator();
+            while (iterator.hasNext()){
+                
+            }
+        }
+    }
+
+    public void remove(int value){
+
+    }
 }
