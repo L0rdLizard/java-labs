7cacbdecc2b2a3aae00475fc1d8c757ce60aa67c
LordLizard Notebook <WarriorXKZ@gmail.com>
2023-03-07 11:58:28 +0300

    lab04 complete

--- src/ru/semestr1/lab04/SortedIntegerList.java
+++ src/ru/semestr1/lab04/SortedIntegerList.java
@@ -5,31 +5,86 @@ import java.util.ListIterator;
 
 public class SortedIntegerList {
     private LinkedList<Integer> list;
-    boolean IsDublicates;
+    boolean allowDublicates;
 
-    public SortedIntegerList(boolean IsDublicates) {
-        this.IsDublicates = IsDublicates;
+    public SortedIntegerList(boolean allowDublicates) {
+        this.allowDublicates = allowDublicates;
         list = new LinkedList<Integer>();
     }
 
     public SortedIntegerList() {
         // this(true);
-        this.IsDublicates = true;
+        this.allowDublicates = true;
     }
     
-    public void add(int value){
-        if (list.isEmpty()){
+    public void add(int value)
+    {
+        if (list.isEmpty())
+        {
             list.add(value);
         }
-        else{
+        else
+        {
             ListIterator<Integer> iterator = list.listIterator();
-            while (iterator.hasNext()){
-                
+            while (iterator.hasNext())
+            {
+                Integer current = iterator.next();
+
+                if (current > value)
+                {
+                  iterator.previous();
+                  iterator.add(value);
+                  return;
+                }
+                if (current == value && !allowDublicates) {
+                  return;
+                }
             }
+            iterator.add(value);
         }
     }
 
     public void remove(int value){
+        ListIterator<Integer> iterator = list.listIterator();
+        while (iterator.hasNext())
+        {
+            Integer current = iterator.next();
+            if (current == value)
+            {
+                iterator.remove();
+                return;
+            }
+        }
+    }
 
+    public boolean equals(SortedIntegerList other) {
+        if (this.list.size() != other.list.size()) {
+            return false;
+        }
+        ListIterator<Integer> iterator1 = this.list.listIterator();
+        ListIterator<Integer> iterator2 = other.list.listIterator();
+        while (iterator1.hasNext()) {
+            if (iterator1.next() != iterator2.next()) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public String toString(){
+        StringBuilder sb = new StringBuilder();
+        sb.append("[");
+
+        ListIterator<Integer> iterator = list.listIterator();
+        while (iterator.hasNext())
+        {
+            sb.append(iterator.next());
+            if (iterator.hasNext())
+            {
+                sb.append(", ");
+            }
+        }
+        sb.append("]");
+        return sb.toString();
     }
 }
