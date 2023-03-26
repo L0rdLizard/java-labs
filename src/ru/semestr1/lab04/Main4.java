49e4d4fac6cf6a08a5c5409a42a092cf44206142
LordLizard <WarriorXKz@gmail.com>
2023-03-26 12:47:26 +0300

    test for lab4

--- src/ru/semestr1/lab04/Main4.java
+++ src/ru/semestr1/lab04/Main4.java
@@ -13,18 +13,28 @@ public class Main4 {
         // System.out.println(list);
         // list.remove(6);
         // System.out.println(list);
-        SortedIntegerList list1 = new SortedIntegerList(false);
+        SortedIntegerList list1 = new SortedIntegerList(true);
         list1.add(1);
         list1.add(2);
         list1.add(3);
         list1.add(4);
-        list1.add(5);
         SortedIntegerList list2 = list1;
 
-        System.out.println(list1);
-        System.out.println(list2);
+        boolean test = list1.equals(list2);
+        System.out.println(test);
 
         SortedIntegerList listresult = list1.cartesianProduct(list2);
         System.out.println(listresult);
+
+//        list1.add(4);
+//        list1.add(5);
+
+//        SortedIntegerList list2 = list1;
+//
+//        System.out.println(listresult);
+//        System.out.println(list1);
+//        System.out.println(list2);
+//
+//        SortedIntegerList listresult = list1.cartesianProduct(list2);
     }
 }
