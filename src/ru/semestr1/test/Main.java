9a3faa69f757b5ff01e760d4c0d3d6c2ba00ed41
LordLizard <WarriorXKz@gmail.com>
2023-03-29 23:30:44 +0300

    Nastya code

--- src/ru/semestr1/test/Main.java
+++ src/ru/semestr1/test/Main.java
@@ -3,21 +3,50 @@ package ru.semestr1.test;
 import java.util.Scanner;
 
 public class Main{ //я писька и я daa
-    public static void main(String[] args) {
+    public static void main(String[] args) { 
+        int yes = 0;
+        int no = 0;
 
-    
-        System.out.println("Я лепешка? Type 'yes' or 'no'");
+        System.out.println("Все привет, меня зовут Анастасия Гетц, и сегодня замечательный день для того чтобы вы увидели все мои навыки программирования. Type 'yes' or 'no'");
         String ar1;
         Scanner in = new Scanner(System.in);
         ar1 = in.nextLine();
+        System.out.println(ar1); 
+        if (ar1.equals("yes")) yes++;
+        if (ar1.equals("no")) no++;
         System.out.println(); 
 
 
-        System.out.println("Ти меня лбюбишь? Type 'yes' or 'no'");
+        System.out.println("Мне, конечно помогал мой любимка Максим, но он чисто иногда запятую ставил в конце..ничего критичного. Type 'yes' or 'no'"); //мииии
         String ar2;
         ar2 = in.nextLine();
+        if (ar2.equals("yes")) yes++;
+        if (ar2.equals("no")) no++;
         System.out.println(); 
 
+        System.out.println("Type 'yes' or 'no'");
+        String ar3; 
+        ar3 = in.nextLine();
+        if (ar3.equals("yes")) yes++;
+        if (ar3.equals("no")) no++;
+        System.out.println();
+
+        System.out.println("Type 'yes' or 'no'");
+        String ar4;
+        ar4 = in.nextLine();
+        if (ar4.equals("yes")) yes++;
+        if (ar4.equals("no")) no++;
+        System.out.println();
+
+        System.out.println("Type 'yes' or 'no'");
+        String ar5;
+        ar5 = in.nextLine();
+        if (ar5.equals("yes")) yes++;
+        if (ar5.equals("no")) no++;
+        System.out.println();
+        
+        System.out.println(yes);
+        System.out.println(no);
         in.close();
         return;
     }
