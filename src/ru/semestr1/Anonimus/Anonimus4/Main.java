3114d400e0fc2f2df9ae363c6063e85d514edb52
LordLizard <WarriorXKz@gmail.com>
2023-04-02 18:20:30 +0300

    anonimus4

--- src/ru/semestr1/Anonimus/Anonimus4/Main.java
+++ src/ru/semestr1/Anonimus/Anonimus4/Main.java
@@ -1,7 +1,17 @@
 package ru.semestr1.Anonimus.Anonimus4;
-
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        
+        Scanner in = new Scanner(System.in); 
+        int n = in.nextInt();
+        in.close();
+
+        if (n < 0) {
+            System.out.println("Дурачёк, введи положительное число");
+        } else {
+            for (int i = 0; i < n; i++) {
+                System.out.println("Я тебя люблю");
+            }
+        }
     }
 }
