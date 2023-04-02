9da50cb82e397e7ac87cc65af09cc1b5e9636e61
LordLizard <WarriorXKz@gmail.com>
2023-04-02 23:09:28 +0300

    6lab complete

---
+++ src/ru/semestr1/lab06/EncodingConverter/EncodingConverter.java
@@ -0,0 +1,64 @@
+package ru.semestr1.lab06.EncodingConverter;
+
+import java.nio.charset.Charset;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import java.io.*;
+
+// Пример вызова:
+// java EncodingConverter in.txt out.txt utf8 cp1251
+
+public class EncodingConverter {
+    public static void main(String[] args) {
+        // check if args length is 4
+        if (args.length != 4) {
+            System.out.println("Invalid number of arguments");
+            return;
+        }
+
+        // check if args[0] and args[1] are valid file paths
+        if (!args[0].matches(".*\\.txt") || !args[1].matches(".*\\.txt")) {
+            System.out.println("Invalid file path");
+            return;
+        }
+
+        // Create a list of all encodings supported by the JVM, including aliases.
+        String[] encodings = Charset.availableCharsets().keySet().toArray(new String[0]);
+        ArrayList<String> charsets = new ArrayList<String>(Arrays.asList(encodings));
+        for (String encoding : encodings) {
+            charsets.addAll(Charset.availableCharsets().get(encoding).aliases());
+        }
+
+        // Check if args[2] and args[3] are valid encodings. Use the original
+        // names for encodings (case-insensitive -> case-sensitive).
+        for (int i = 2; i < 4; i++) {
+            final int j = i;
+            try {
+                args[j] = charsets.stream().filter(charset -> charset.equalsIgnoreCase(args[j])).findFirst().get();
+            } catch (Exception e) {
+                System.out.println("Invalid encoding");
+                return;
+            }
+        }
+
+        try {
+            convert(new File(args[0]), new File(args[1]), args[2], args[3]);
+        } catch (IOException e) {
+            System.out.println("Error while converting file");
+        }
+    }
+
+    public static void convert(File source, File target, String fromEncoding, String toEncoding) throws IOException {
+        try (
+                BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(source), fromEncoding));
+                BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(target), toEncoding));) {
+            char[] buffer = new char[1024];
+            int read;
+            while ((read = br.read(buffer)) != -1) {
+                bw.write(buffer, 0, read);
+            }
+        }
+    }
+}
+
--- src/ru/semestr1/lab06/Main6.java
+++ src/ru/semestr1/lab06/Main6.java
@@ -1,7 +1,20 @@
 package ru.semestr1.lab06;
 
+import ru.semestr1.lab06.FormattedInput.FormattedInput;
+
 public class Main6 {
     public static void main(String[] args) {
-        
+        Object[] vals = FormattedInput.scanf("%d %s %c");
+        int i = (int) vals[0];
+        String s = (String) vals[1];
+        char c = (char) vals[2];
+        System.out.println("i = " + i + ", s = " + s + ", c = " + c);
+
+        String str = "123 456 789";
+        Object[]  valsStr = FormattedInput.sscanf("%d %d %d", str);
+        int x = (int) valsStr[0];
+        int y = (int) valsStr[1];
+        int z = (int) valsStr[2];
+        System.out.println("x = " + x + ", y = " + y + ", z = " + z);
     }
 }
