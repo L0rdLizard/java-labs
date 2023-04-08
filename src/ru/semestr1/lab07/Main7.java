ddbc840b6cc9b0d19754c7a58a56bfa8273cdd6b
LordLizard <WarriorXKz@gmail.com>
2023-04-08 22:27:59 +0300

    lab07 try to fix

--- src/ru/semestr1/lab07/Main7.java
+++ src/ru/semestr1/lab07/Main7.java
@@ -1,4 +1,31 @@
 package ru.semestr1.lab07;
 
+import java.io.*;
+
 public class Main7 {
+    public static void main(String[] args) {
+        Settings test1 = new Settings();
+        test1.put("Volume", 10);
+        test1.put("Brightness", 8);
+        System.out.println(test1);
+        String basePath = "src/ru/semestr1/lab07/";
+//        test1.saveToBinaryFile(basePath + "TestSaveToBin");
+        try{
+            test1.saveToBinaryFile(basePath + "TestSaveToBin");
+            Settings test2 = new Settings();
+            test2.loadFromBinaryFile("TestSaveToBin");
+            System.out.println(test2);
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+
+//        try{
+//            test1.saveToTextFile(basePath + "TestSaveToText");
+//            Settings test3 = new Settings();
+//            test3.loadFromTextFile(basePath + "TestSaveToText");
+//            System.out.println(test3);
+//        } catch (IOException e) {
+//            System.out.println(e.getMessage());
+//        }
+    }
 }
--- src/ru/semestr1/lab07/Settings.java
+++ src/ru/semestr1/lab07/Settings.java
@@ -24,17 +24,52 @@ public class Settings {
             out.flush();
             out.close();
         } catch (Exception e) {
-            throw new IOException("Error while loading from binary file: " + e.getMessage());
+            throw new IOException("Error while saving to binary file: " + e.getMessage());
         }
     }
     public void loadFromBinaryFile(String filename) throws IOException {
-
+        try {
+            ObjectInputStream in = new ObjectInputStream(new FileInputStream(filename));
+            Settings settings = (Settings) in.readObject();
+            in.close();
+            this.settingsMap = settings.settingsMap;
+        } catch (Exception e) {
+            throw new IOException("Error while loading from binary file: " + e.getMessage());
+        }
     }
     public void saveToTextFile(String filename) throws IOException {
-
+        try {
+            PrintWriter out = new PrintWriter(new FileWriter(filename));
+            out.println(this);
+            out.flush();
+            out.close();
+        } catch (Exception e) {
+            throw new IOException("Error while saving to text file: " + e.getMessage());
+        }
     }
     public void loadFromTextFile(String filename) throws IOException {
-
+//        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(filename))) {
+//            String line;
+//            while ((line = bufferedReader.readLine()) != null) {
+//                String[] keyValue = line.split(" ");
+//                settingsMap.put(keyValue[0], Integer.parseInt(keyValue[1]));
+//            }
+//        } catch (IOException e) {
+//            throw new IOException("Error while reading from file", e);
+//        }
+        try {
+            BufferedReader in = new BufferedReader(new FileReader(filename));
+            String line = in.readLine();
+            in.close();
+            this.settingsMap = new HashMap<String, Integer>();
+            String[] pairs = line.substring(10, line.length() - 1).split(", ");
+            for (String pair : pairs) {
+                String[] keyValue = pair.split("=");
+                this.settingsMap.put(keyValue[0], Integer.parseInt(keyValue[1]));
+            }
+        } catch (Exception e) {
+            throw new IOException("Error while loading from text file: " + e.getMessage());
+        }
     }
 
     @Override
